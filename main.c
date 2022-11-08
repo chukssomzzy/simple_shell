60b137ba957c79083b3b66728a958dc27311601e
chukssomzzy <chukssomzzy19@gmail.com>
2022-11-08 14:09:13 +0100

    setup and task2

---
+++ main.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+
+
+/**
+ * main - starting point for execution
+ * @argc: Size of argv
+ * @argv: Contains cmd args
+ * Return: 0 (success())
+ */
+
+
+int main(int argc, char **argc, char **argp)
+{
+
+}
