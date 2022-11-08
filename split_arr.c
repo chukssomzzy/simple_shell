054b441bcd308ddb6045202cd35c400df27c3e0a
chuk'ssomzzy <chukssomzzy19@gmail.com>
2022-11-08 07:39:41 -0800

    split element in an array

--- split_arr.c
+++ split_arr.c
@@ -1,12 +1,31 @@
 # include "main.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * split_t_arr - split string into array
  * @s: string to split
+ * @dl: delimiter
  * Return: pointer to the string
  */
 
-int split_t_arr(char *s, char *dl)
+char **split_t_arr(char *s, const char *dl)
 {
+	char **argv;
+	size_t n = 100;
+	size_t i = 1;
+	char *str;
 
+	argv = malloc(sizeof(*argv) * n);
+	for (str = s; ; str = NULL, i++)
+	{
+		if (i == n)
+		{
+			i = 10;
+			n += i;
+			argv = realloc(argv, sizeof(*argv) * n);
+		}
+		*argv = strtok(str, dl);
+	}
+	return (argv);
 }
