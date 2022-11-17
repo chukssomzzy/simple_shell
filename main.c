3435c33c0306413d9a5dda925880eb6453cd4f89
chukssomzzy <chukssomzzy19@gmail.com>
2022-11-17 11:32:57 +0100

    cntr d

--- main.c
+++ main.c
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
 		lnr = getline(&line, &n, stdin);
 		if (lnr == -1)
 		{
-			perror("getline");
+			dprintf(STDOUT_FILENO, "\n");
 			exit(1);
 		}
 		if (lnr == 1)
