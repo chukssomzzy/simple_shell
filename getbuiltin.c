aebceb152d127cd4db6f4a24af420c9a24417deb
chukssomzzy <chukssomzzy19@gmail.com>
2022-11-17 12:01:08 +0100

    rm strcmp

--- getbuiltin.c
+++ getbuiltin.c
@@ -1,5 +1,5 @@
 # include "main.h"
-
+int _strcmp(char *s, char *t);
 /**
  * getbuiltin - return builtin
  * @s: string to cmp
@@ -17,9 +17,30 @@ int (*getbuiltin(char *s))(void)
 
 	while ((builtin + i)->s)
 	{
-		if (strcmp(s, (builtin + i)->s) == 0)
+		if (_strcmp(s, (builtin + i)->s) == 0)
 			return (*builtin->cmdfunc);
 		i++;
 	}
 	return (NULL);
+}
+
+/**
+ * _strcmp - compares two string
+ * @s: pointer to string
+ * @t: pointer to second string
+ * Return: 0 if equal
+ */
+
+int _strcmp(char *s, char *t)
+{
+	size_t i = 0;
+
+	while (*(s + i) == *(t + i))
+	{
+		if (*(s + i) == '\0')
+			return (0);
+		i++;
+	}
+
+	return (*(s + i) - *(t + i));
 }
