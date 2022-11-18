4c98e033fd034a3a80a3945333fae85c0a9159c4
chukssomzzy <chukssomzzy19@gmail.com>
2022-11-18 14:25:08 +0100

    learnt all

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-# simple_shell
-A simple shell
-# About
+![alt text](https://s3.amazonaws.com/intranet-projects-files/holbertonschool-low_level_programming/235/shell.jpeg)
+
+### Description
+
+Ella and Alexxy Shell is a simple UNIX command interpreter that replicates functionalities of the simple shell (sh). Additional functions are also included. This program was written entirely in C as a milestone project for ALX Africa Software Engineering.
+
+### Installation
+
+Clone this repository into your working directory. For best results, files should be compiled with GCC and the following flags: -Wall -Wextra -Werror -pedantic -std=gnu89
+
+### Usage
+
+After compilation, the resulting program can run stand-alone, either in interactive or non-interactive mode.
+
+#### Interactive Mode
+
+In interactive mode, simply run the program and wait for the prompt to appear. From there, you can type commands freely, exiting with either the "exit" command or ctrl-D.
+
+#### Non-Interactive Mode
+
+In non-interactive mode, echo your desired command and pipe it into the program like this:
+
+```sh
+echo "ls" | ./shell
+```
+
+In non-interactive mode, the program will exit after finishing your desired command(s).
--- main.c
+++ main.c
@@ -1,125 +1,73 @@
-# include "main.h"
-# include <stdio.h>
-#include <stdlib.h>
-# include <unistd.h>
-# include <sys/stat.h>
-# define DELIM " \n"
-static void free_buf(char **lines, char **line);
-static size_t word_count(char *b);
+#include "main.h"
 
 /**
- * main - starting point for execution
- * @argc: Size of argv
- * @argv: Contains cmd args
- * Return: 0 (successe))
+ * free_data - frees data structure
+ *
+ * @datash: data structure
+ * Return: no return
  */
-
-
-int main(int argc, char **argv)
+void free_data(data_shell *datash)
 {
-	char *line = NULL, *p = argv[argc - 1];
-	size_t n;
-	ssize_t lnr;
-	size_t arr_size = 1;
-	char **lines;
-	int (*f)(void);
+	unsigned int i;
 
-	do {
-		print_line(NULL);
-		lnr = getline(&line, &n, stdin);
-		if (lnr == -1)
-		{
-			dprintf(STDOUT_FILENO, "\n");
-			exit(1);
-		}
-		if (lnr == 1)
-			continue;
-		arr_size += word_count(line);
-		lines = split_t_arr(line, DELIM, &arr_size);
-		lines[0] = getpath(lines[0]);
-		if (!lines)
-		{
-			perror("split_t_arr");
-			exit(1);
-		}
-		f = getbuiltin(lines[0]);
-		if ((f))
-		{
-			free_buf(lines, &line);
-			f();
-			continue;
-		}
-
-		if (check_dir(lines[0], p))
-		{
-			free_buf(lines, &line);
-			continue;
-		}
-		if ((shell_cntrl(lines)))
-		{
-			perror("shell_cntrl");
-			exit(1);
-		}
-		free_buf(lines, &line);
-	} while (lnr != EOF);
-	exit(EXIT_SUCCESS);
-}
-
-/**
- * free_buf - free free_buf
- * @lines: buf to free
- * @line: Number of pointer in free_buf
- */
-
-void free_buf(char **lines, char **line)
-{
-	if ((*lines - *line) != 0)
-		free(lines[0]);
-	free(*line);
-	free(lines);
-	*line = NULL;
-}
-
-/**
- * check_dir - find referred file
- * @p: pathname to folder
- * @c: pathname to calling process
- * Return: 0(True)
- */
-
-int check_dir(char *p, char *c)
-{
-	struct stat st;
-	static int cnt = 1;
-
-	if (p && stat(p, &st) == 0)
-		return (0);
-	if (c)
-		dprintf(STDERR_FILENO, "%s: %i: %s: not found\n", c, cnt++, p);
-	return (1);
-}
-
-/**
- * word_count - find number of word in buffer
- * @b: buffer
- * Return: number of word
- */
-
-size_t word_count(char *b)
-{
-	size_t n = 0, state = 0;
-	char *s = b;
-
-	while (*s)
+	for (i = 0; datash->_environ[i]; i++)
 	{
-		if ((*s == ' ' || *s == '\t' || *s == '\n') && !state)
-		{
-			state = 1;
-			n++;
-		}
-		else
-			state = 0;
-		s++;
+		free(datash->_environ[i]);
 	}
-	return (n);
+
+	free(datash->_environ);
+	free(datash->pid);
+}
+
+/**
+ * set_data - Initialize data structure
+ *
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
+ */
+void set_data(data_shell *datash, char **av)
+{
+	unsigned int i;
+
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
